861ce98daa045cf1be4995a097e9469976c547f1
matiasjokela <matias.jokela@outlook.com>
2021-12-07 18:51:13 +0200

    Seems to work now, but is ugly

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,6 @@
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 
 static void	read_fd(const int fd, char **line, char *str_arr[FD]);
@@ -18,12 +19,12 @@ static void	read_buffer(const int fd, char *str_arr[FD])
 		return ;
 	}
 	
+	ft_bzero(buf, BUFF_SIZE + 1);
 	bytes_read = read(fd, buf, BUFF_SIZE);
 	tmp = ft_strjoin(str_arr[fd], buf);
 	free(str_arr[fd]);
 	str_arr[fd] = ft_strdup(tmp);
 	free(tmp);
-	//buf[bytes_read] = '\0'; //???????
 	if (bytes_read == 0)
 		str_arr[FD - 3] = "1";
 
@@ -34,16 +35,18 @@ static void	read_fd(const int fd, char **line, char *str_arr[FD])
 	int		i;
 	char	*tmp;
 
+
 	i = 0;
 	while ((str_arr[fd]) && str_arr[fd][i])
 	{
 		if (str_arr[fd][i] == '\n')
 		{
+			
 			str_arr[fd][i] = '\0';
 			*line = ft_strdup(str_arr[fd]);
 			tmp = ft_strdup(&(str_arr)[fd][i + 1]);
 			if (tmp[0] == 0 && ft_atoi(str_arr[FD - 3]) == 1)
-				str_arr[FD - 1] = "0";
+				str_arr[FD - 3] = "2";
 			free(str_arr[fd]);
 			str_arr[fd] = ft_strdup(tmp);
 			free(tmp);
@@ -60,7 +63,8 @@ static void	read_fd(const int fd, char **line, char *str_arr[FD])
 			str_arr[fd] = ft_strdup(tmp);
 			free(tmp);
 			str_arr[FD - 2] = "1";
-			str_arr[FD - 1] = "0";
+			str_arr[FD - 3] = "2";
+
 	}
 }
 
@@ -71,20 +75,33 @@ int			get_next_line(const int fd, char **line)
 {
 	static char	*str_arr[FD];
 
+
+
 	str_arr[FD - 1] = "1"; // return value
 	str_arr[FD - 2] = "0"; // found line
-	str_arr[FD - 3] = "0"; // Reached EOF 0 no, 1 yes, 2 last line returned
+	if (!str_arr[FD - 3])
+		str_arr[FD - 3] = "0"; // Reached EOF 0 no, 1 yes, 2 last line returned
+
 
 	if (ft_atoi(str_arr[FD - 3]) == 2)
+	{
+		*line = NULL;
+		str_arr[FD - 3] = "0";
 		return (0);
+	}
+
+
 
 	while (ft_atoi(str_arr[FD - 2]) == 0)
 	{
 		if (ft_atoi(str_arr[FD - 3]) == 0)
 			read_buffer(fd, str_arr);
+
 		read_fd(fd, line, str_arr);
+
 	} 
 
+	
 	return (ft_atoi(str_arr[FD - 1]));
 
 
@@ -93,7 +110,7 @@ int			get_next_line(const int fd, char **line)
 
 int main(void)
 {
-	char **s;
+	char *s;
 	int x;
 
 	int fd = open("todo.txt", O_RDONLY);
@@ -102,8 +119,10 @@ int main(void)
 	printf("File 1:\n");
 	while (1)
 	{
-		x = get_next_line(fd, s);
-		printf("%d, %s\n", x, *s);
+
+		x = get_next_line(fd, &s);
+		
+		printf("%d, %s\n", x, s);
 		if (x <= 0)
 			break ;
 
@@ -111,15 +130,13 @@ int main(void)
 	printf("-----\nFile 2:\n");	
 	while (1)
 	{
-		x = get_next_line(fd1, s);
-		printf("%d, %s\n", x, *s);
+		x = get_next_line(fd1, &s);
+		printf("%d, %s\n", x, s);
 		if (x <= 0)
 			break ;
 	}
 			
 
 
-
-	return (0);
 
 }
